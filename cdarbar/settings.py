50aaddabd36bc04db0ab2841d442f1abc4d75ad9
alam-mahtab <67095487+alam-mahtab@users.noreply.github.com>
2020-12-08 22:51:26 +0530

    db change

--- cdarbar/settings.py
+++ cdarbar/settings.py
@@ -78,11 +78,13 @@ WSGI_APPLICATION = 'cdarbar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cdarbar',
-        'USER' : 'postgres',
-        'PASSWORD' : '1234' ,
-        'HOST' : 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'cdarbar',
+        # 'USER' : 'postgres',
+        # 'PASSWORD' : '1234' ,
+        # 'HOST' : 'localhost',
     }
 }
 
